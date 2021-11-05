e886dfa19bcb25bc2999cce4f9f18bc7d8a57673
Ayako Mikami <ayakomikami@Ayakos-iMac.local>
2021-11-05 16:43:40 -0400

    ErrorBoundary and Redirect

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,36 @@
+//mostly took this from the React docs
+
+import { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, redirect: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    // log this to Sentry, Azure Monitor, NewRelic, TrackJS
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h2>
+          We have an error. <Link to="/">Click here</Link> to go back to home
+          page. Or wait 5 seconds.
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
